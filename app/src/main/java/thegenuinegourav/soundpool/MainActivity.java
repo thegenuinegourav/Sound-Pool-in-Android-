82d23dd7ac3c96623b938e2251c46244231c89c1
thegenuinegourav <gourav96suri@gmail.com>
2017-11-19 11:52:41 +0530

    Initial Commit

---
+++ app/src/main/java/thegenuinegourav/soundpool/MainActivity.java
@@ -0,0 +1,49 @@
+package thegenuinegourav.soundpool;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.media.SoundPool.OnLoadCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SoundPool soundPool;
+    private int soundID;
+    boolean loaded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        // Load the sound
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+        soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId,
+                                       int status) {
+                loaded = true;
+            }
+        });
+        soundID = soundPool.load(this, R.raw.punch, 1);
+
+    }
+
+    public void Clicked(View view) {
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        float actualVolume = (float) audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = (float) audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = actualVolume / maxVolume;
+        // Is the sound loaded already?
+        if (loaded) {
+            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+        }
+    }
+}
